b4b3f9959346f2da70fa6785d669880e40e6ffcd
facccge <fcfc5702@qq.com>
2019-04-23 12:08:40 +0800

    Completed test cases of InputValidator.validate

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -1,8 +1,35 @@
 package tw.core;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import tw.validator.InputValidator;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
 /**
  * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
  */
 public class InputValidatorTest {
+    private static InputValidator inputValidator;
 
+    @BeforeClass
+    public static void init(){
+        inputValidator = new InputValidator();
+    }
+
+    @Test
+    public void validateSuccess(){
+        String input = "8 4 2 1";
+        Boolean actual = inputValidator.validate(input);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void validateFailure(){
+        String input = "9999";
+        Boolean actual = inputValidator.validate(input);
+        assertFalse(actual);
+    }
 }
