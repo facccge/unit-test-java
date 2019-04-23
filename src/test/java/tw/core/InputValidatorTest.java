e1ff64bfcd56c2faf441c186c64a82e7af0f944e
facccge <fcfc5702@qq.com>
2019-04-23 12:16:08 +0800

    Updated test cases of InputValidator.validate

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -3,10 +3,16 @@ package tw.core;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mock;
 import tw.validator.InputValidator;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
@@ -22,14 +28,14 @@ public class InputValidatorTest {
     @Test
     public void validateSuccess(){
         String input = "8 4 2 1";
-        Boolean actual = inputValidator.validate(input);
+        boolean actual = inputValidator.validate(input);
         assertTrue(actual);
     }
 
     @Test
     public void validateFailure(){
         String input = "9999";
-        Boolean actual = inputValidator.validate(input);
+        boolean actual = inputValidator.validate(input);
         assertFalse(actual);
     }
 }
