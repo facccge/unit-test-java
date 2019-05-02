89dbfda34d17b045205a8bc417e95524788cdbe8
facccge <fcfc5702@qq.com>
2019-05-02 11:04:14 +0800

    Completed test cases of Answer.createAnswer

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,8 +1,38 @@
 package tw.core;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static tw.core.Answer.*;
+
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
  */
 public class AnswerTest {
+    @Test
+    public void createAnswerTest(){
+        String input = "8 4 2 1";
+        Answer actual = createAnswer(input);
+        Answer expected = new Answer();
+        List list = new ArrayList<>();
+        list.add("8");
+        list.add("4");
+        list.add("2");
+        list.add("1");
+        expected.setNumList(list);
+        assertEquals(expected.toString(),actual.toString());
+    }
 
+    @Test
+    public void checkTest(){
+
+    }
+
+    @Test
+    public void validateTest(){
+
+    }
 }
